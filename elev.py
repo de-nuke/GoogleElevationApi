d9bb141def51875bf220145e4cd9cf21d658874c
de_nuke <sut.michal@gmail.com>
2017-07-12 22:46:33 +0200

    Refactoring + example usage in --help

--- elev.py
+++ elev.py
@@ -4,66 +4,61 @@ import argparse
 import sys
 from argparse import RawTextHelpFormatter
 
+
 API_KEY ='AIzaSyAN7atisvzb23HTyefcAKP9nc5nwNAe9HA'
-LOC_PER_REQ = 256 # safe amount. 512 would be accepted, but URLs are too long then ( over 8096 characters )
+LOC_PER_REQ = 256   # safe amount. 512 would be accepted, but URLs are too long then ( over 8096 characters )
+                    # Max available value is calculated, so 256 will be replaced anyway
+
 
 parser = argparse.ArgumentParser(description="""
-*******
-Returns elevation values for input locations. Required input format is:
-lat1,lng1
-lat2,lng2
-...
-etc.
+Example usages:
+1. python elev.py 20170607154138.csv out.csv
+    Takes input from 20170607154138.csv and saves the output in out.csv.
+2. python elev.py 20170607154138.csv -
+    Takes input from 20170607154138.csv and prints the output to stdout.
 
-example:
-52.226488,21.001362
-52.211878,20.962028
-52.268324,20.800880
+Optional flags:
 
-Any whitespaces beetwen numbers and comma or at the begining and end of line are omitted.
-*******
+--no_status - DOES NOT print how many locations were processed (No option means printing info)
+--only_elev - Output contain only elevation values.
+
 """, formatter_class=RawTextHelpFormatter)
 parser.add_argument('input_file', help="File with input locations.")
 parser.add_argument('output_file', help="File which results will be written to. For standard output type: -")
 parser.add_argument('--only_elev', help="Only elevation values are printed to output file", action="store_true")
 parser.add_argument('--comma_style', help="[NOT IMPLEMENTED YET]Prints floating point values with comma, and space as a separator", action="store_true")
 parser.add_argument('--no_status', help="With this flag, script WILL NOT print out how many locations were processed", action="store_true")
-action = parser.add_mutually_exclusive_group(required=False)
-action.add_argument('--sort_lat_asc', help="[NOT IMPLEMENTED YET]Sorting points ascending by lattitude.", action="store_true")
-action.add_argument('--sort_lat_desc', help="[NOT IMPLEMENTED YET]Sorting points descending by lattitude.", action="store_true")
-action.add_argument('--sort_lng_asc', help="[NOT IMPLEMENTED YET]Sorting points ascending by longtitude.", action="store_true")
-action.add_argument('--sort_lng_desc', help="[NOT IMPLEMENTED YET]Sorting points descending by longtitude.", action="store_true")
-action.add_argument('--sort_elev_asc', help="[NOT IMPLEMENTED YET]Sorting points ascending by elevation.", action="store_true")
-action.add_argument('--sort_elev_desc', help="[NOT IMPLEMENTED YET]Sorting points descending by elevation.", action="store_true")
 
 args = parser.parse_args()
 
 locations = []
 elevations = []
 
-# Imports data from file
+# Import data from file
 with open(args.input_file) as f:
-    f.readline() # skips first line
+    f.readline() # skip first line
     for line in f.readlines():
         line = line.strip(' \n\r\t')
         lat, lng = line.split(',')[1], line.split(',')[2]
         lat, lng = lat.strip(), lng.strip()
         locations.append( (lat,lng) )
 
+
+# Calculate max available amount of locations in one request
 chars_left = 8096 - len("https://maps.googleapis.com/maps/api/elevation/json?locations=") - len("&key=" + API_KEY)
 LOC_PER_REQ = int(chars_left/len(lat + ',' + lng + '|'))
 
 
-# Calculates number of requests required to complete the task
+# Calculate number of requests required to complete the task
 number_of_requests = int(len(locations)/LOC_PER_REQ) + 1
 
-# Gets elevation values for every location
 
+# Get elevation values for every location
 counter = 0
 for i in range(number_of_requests):
     counter += len(locations[i*LOC_PER_REQ:(i+1)*LOC_PER_REQ])
     
-    # Creates URL string
+    # Create URL string
     url = "https://maps.googleapis.com/maps/api/elevation/json?locations="
     for location in locations[i*LOC_PER_REQ:(i+1)*LOC_PER_REQ]:
         lat, lng = location
@@ -71,8 +66,7 @@ for i in range(number_of_requests):
     url = url[:-2]
     url += '&key=' + API_KEY
 
-    
-    # Gets responses and appends elevations to the list
+    # Get responses and append elevations to the list
     response = json.loads(urllib.request.urlopen(url).read())
     
     for result in response['results']:
@@ -81,33 +75,32 @@ for i in range(number_of_requests):
     if not args.no_status:
         print('Processed:',counter,"locations") #console log
 
-# Makes sure that we got elevation value for each location
+
+# Make sure that we got elevation value for each location
 assert len(locations) == len(elevations)
 
-points = list(zip(locations, elevations))
 
-
+# Print output
 if args.output_file is not '-' :
     file_out = open(args.output_file, 'w')
 else:
     file_out = sys.stdout
 
-with open(args.input_file) as file_in:
+with open(args.input_file) as file_in: # Open input file again to rewrite lines
+
+    # First line (headers)
     line = file_in.readline()
     line = line.replace('\n', '') if '\n' in line else line
     print(line + ',elevationfixed', file=file_out)
-    
-    for point in points:
-        lat, lng = point[0]
-        elevation = str(point[1])
-    
+
+    # Data
+    for elevation in elevations:
         if args.only_elev:
-            print(elevation, file=file_out)
+            print(str(elevation), file=file_out)
         else:
             line = file_in.readline()
             line = line.replace('\n', '') if '\n' in line else line
-            print(line + ',' + elevation, file=file_out)
-##            print(file_in.readline() + elevation, file=file_out)
-
-    file_out.close()
+            print(line + ',' + str(elevation), file=file_out)
+            
+file_out.close()
 
